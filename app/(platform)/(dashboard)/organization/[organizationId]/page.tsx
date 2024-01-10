fe39bec6e4be27a87a1bf7042b01c500ada4d0cf
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2024-01-10 13:52:07 +0100

    added info and removed old form

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,20 +1,11 @@
-import { create } from "@/actions/create-board/create-board";
-import { Button } from "@/components/ui/button";
-import { db } from "@/lib/db";
-import { Board } from "./board";
-import { Form } from "./form";
+import { Info } from "./_components/info";
+
 
 const OrganizationIdPage = async () => {
-  const boards = await db.board.findMany();
 
   return (
-    <div className="flex flex-col space-y-4">
-      <Form />
-      <div className="space-y-2">
-        {boards.map((board) => (
-          <Board key={board.id} title={board.title} id={board.id} />
-        ))}
-      </div>
+    <div className="w-full mb-20">
+      <Info />
     </div>
   );
 };

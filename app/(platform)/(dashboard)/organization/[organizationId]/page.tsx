34a2b55c81ff1e2b9d7df644f98c1f694785d1e6
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2024-01-10 14:18:43 +0100

    added board list

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,3 +1,5 @@
+import { Separator } from "@/components/ui/separator";
+import { BoardList } from "./_components/board-list";
 import { Info } from "./_components/info";
 
 
@@ -6,6 +8,10 @@ const OrganizationIdPage = async () => {
   return (
     <div className="w-full mb-20">
       <Info />
+      <Separator className="my-4"/>
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
     </div>
   );
 };

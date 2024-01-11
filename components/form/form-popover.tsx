c3ec2b136346f33e2c95005e4eae3ed4bb83c110
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2024-01-11 12:16:25 +0100

    added close after submit  added popover to navbar button

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -14,6 +14,8 @@ import { Button } from "@/components/ui/button";
 import { X } from "lucide-react";
 import { toast } from "sonner";
 import { FormPicker } from "./form-picker";
+import { ElementRef, useRef } from "react";
+import { useRouter } from "next/navigation";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -28,13 +30,16 @@ export const FormPopover = ({
   align,
   sideOffset = 0,
 }: FormPopoverProps) => {
+  const router = useRouter();
+  const closeRef = useRef<ElementRef<"button">>(null);  
+
   const { execute, fieldErrors } = useAction(createBoard, {
     onSuccess: (data) => {
-      console.log({ data });
       toast.success("Board created");
+      closeRef.current?.click();
+      router.push(`/board/${data.id}`);
     },
     onError: (error) => {
-      console.log({ error });
       toast.error(error);
     },
   });
@@ -58,7 +63,7 @@ export const FormPopover = ({
         <div className="text-sm font-medium text-center text-neutral-600">
           Create board
         </div>
-        <PopoverClose>
+        <PopoverClose ref={closeRef} asChild>
           <Button
             variant="ghost"
             className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600"

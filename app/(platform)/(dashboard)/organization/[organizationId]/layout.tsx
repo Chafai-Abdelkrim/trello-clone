1c09b24adb77a467f7f5536d9214074bcdaf47bf
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2024-01-11 13:57:59 +0100

    updated BoardList and title metatags

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,12 +1,22 @@
 import { OrgControl } from "./_components/org-controler";
+import { auth } from "@clerk/nextjs";
+import { startCase } from "lodash";
 
-const OrganizationIdLayout = ({children} : {children: React.ReactNode}) => {
-    return (
-        <>
-            <OrgControl />
-            {children}
-        </>
-    )
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
 }
+
+const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  );
+};
 
 export default OrganizationIdLayout;

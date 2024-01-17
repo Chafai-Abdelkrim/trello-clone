0953953b0b278ac386db0e5094612cd4068da9e2
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2024-01-17 13:15:11 +0100

    updated card model and added card header

--- components/modals/card-modal/index.tsx
+++ components/modals/card-modal/index.tsx
@@ -1,22 +1,27 @@
-"use client"
+"use client";
 
 import { Dialog, DialogContent } from "@/components/ui/dialog";
-import { useCardModal } from "@/hooks/use-card-modal"
-
+import { useCardModal } from "@/hooks/use-card-modal";
+import { fetcher } from "@/lib/fetcher";
+import { CardWithList } from "@/types";
+import { useQuery } from "@tanstack/react-query";
+import { Header } from "./header";
 
 export const CardModal = () => {
-    const id = useCardModal((state) => state.id);
-    const isOpen = useCardModal((state) => state.isOpen);
-    const onClose = useCardModal((state) => state.onClose);
+  const id = useCardModal((state) => state.id);
+  const isOpen = useCardModal((state) => state.isOpen);
+  const onClose = useCardModal((state) => state.onClose);
 
-    return (
-        <Dialog 
-            open={isOpen}
-            onOpenChange={onClose}
-        >
-            <DialogContent>
-                I am a modal
-            </DialogContent>
-        </Dialog>
-    )
-}
+  const { data: cardData } = useQuery<CardWithList>({
+    queryKey: ["card", id],
+    queryFn: () => fetcher(`/api/cards/${id}`),
+  });
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent>
+        {!cardData ? <Header.Skeleton /> : <Header data={cardData} />}
+      </DialogContent>
+    </Dialog>
+  );
+};

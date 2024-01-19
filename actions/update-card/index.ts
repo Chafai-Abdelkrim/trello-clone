f405ed9933b51b2a2ce369291abcfbb7f143c0ed
Chafai-Abdelkrim <chafai.abdelkrim@gmail.com>
2024-01-19 11:51:49 +0100

    changed all action to work with activity

--- actions/update-card/index.ts
+++ actions/update-card/index.ts
@@ -1,8 +1,10 @@
 "use server";
 
+import { createAuditLog } from "@/lib/create-audit-log";
 import { createSafeAction } from "@/lib/create-safe-action";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 import { UpdateCard } from "./schema";
 import { InputType, ReturnType } from "./types";
@@ -34,6 +36,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         ...values,
       },
     });
+
+    await createAuditLog({
+      entityTitle: card.title,
+      entityId: card.id,
+      entityType: ENTITY_TYPE.CARD,
+      action: ACTION.UPDATE,
+    })
   } catch (error) {
     return {
       error: "Failed to update",
